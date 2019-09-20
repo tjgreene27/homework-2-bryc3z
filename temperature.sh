a97d0b45ee27ad10ce1942738c1ee0e64024b89b
Tyler Greene <tjgreene@ksu.edu>
2019-09-19 19:17:25 -0500

    edited temperature.sh. did not recieve inputs before.

--- temperature.sh
+++ temperature.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #converts Fahrenheit to Celsius
+echo "Enter Fahrenheit temperature:"
+read F
+export C=$(echo "scale=4;($F-32)*(0.5555)"|bc)
+echo "That is $C degrees Celsius"
 
-export C=$(echo "scale=4;($1-32)*(0.5555)"|bc)
-echo "That is $C Celsius"
+#converts input also to Kelvin
+
+export K=$(echo "scale=1;($C + 273.15)"|bc)
+echo "That is also $K Kelvin"
+
+
